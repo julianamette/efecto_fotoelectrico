f4f5d75b3d06258a8c7c1599f73bbd88965dc62a
julianamette <julianamette3@gmail.com>
2019-05-26 12:36:44 -0300

    Add files via upload

--- efecto_fotoelectrico.py
+++ efecto_fotoelectrico.py
@@ -12,6 +12,7 @@ from lmfit import minimize, Minimizer, Parameters, Parameter, report_fit
 import simu as s
 import time
 import simu2 as s2
+from lmfit.models import SkewedGaussianModel
 
 def gaussiana(sigma, mu, a, x):
     return a * np.exp(-0.5 * ( (x - mu) / sigma )**2)
@@ -58,6 +59,12 @@ def fit_gauss(led_esp, led ):
     result = model.fit(led_esp[led][:,1], x = led_esp[led][:,0], nan_policy = 'propagate')
     return result
 
+def fit_skewedgauss(led_esp, led ):
+    model = SkewedGaussianModel()
+    params = model.make_params(amplitude=1, center=400, sigma=5, gamma=1)
+    result = model.fit(led_esp[led][:,1],params, x = led_esp[led][:,0], nan_policy = 'propagate')
+    return result
+
 def fit_act(led_volt, led):
     model = Model(act, independent_vars = ['x'])
     model.set_param_hint('xo', value = -1, vary = True)
@@ -82,6 +89,13 @@ y = y/np.sum(y)
 x = 300000e12/x
 v = np.linspace(-2,2,1000)
 
+def set(led):
+    x = led_esp[led][:,0]
+    y = led_esp[led][:,1]
+    y = y/np.sum(y)
+    x = 300000e12/x
+    return x, y 
+
 def simular(n,v,x,y,h):
     start = time.time()
     g = s.simu(n = n, v = v , phi = 2.3, probas = y, frec = x, h = h)
@@ -96,9 +110,42 @@ def simular2(n,v,x,y,h, plot = 'si'):
     g = s2.simulacion(n = n, v = v , phi = 2.3, proba = y, frec = x, h = h)
     stop = time.time()
     print(stop-start)
-    plt.plot(v,g,'.')
+    plt.plot(v,g,'.-')
     if plot == 'si':
         plt.show()
     return g
 
-extraccion_vo()
+#extraccion_vo()
+def comparar_sim_sigma(array_sigmas):
+    for i in array_sigmas:
+        x = np.linspace(100,1000,5000)
+        y = gaussiana(i, 485, 1,x)
+        y = y/np.sum(y)
+        x = 300000e12/x
+        v = np.linspace(-2,2,1000)
+        g = simular2(100000,v,x,y,h, plot = 'no')
+    plt.show()
+
+e = 1.602176565e-19
+def derivar(led_volt,led):
+    dg = np.diff(led_volt[led][:,1])/np.diff(led_volt[led][:,0]) #* (h/e) 
+    plt.plot(led_volt[led][:,0][:-1],dg,label = 'derivado')
+    plt.plot(led_volt[led][:,0],led_volt[led][:,1],label = 'normal')
+    plt.title(led)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+def max_der(led):
+    plt.plot(led_volt[led][:,0][:-1],np.diff(led_volt[led][:,1])*20,'.')
+    plt.plot(led_volt[led][:,0],led_volt[led][:,1],'.')
+    arg = np.argmax(np.diff(led_volt[led][:,1]))
+    plt.plot(led_volt[led][:,0][arg],led_volt[led][:,1][arg],'r*')
+    plt.show()
+
+for led in led_volt:
+    max_der(led)
+
+
+
